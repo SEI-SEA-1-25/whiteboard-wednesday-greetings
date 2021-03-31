624873b978c5547287e187cb5ff38b67f2fa0316
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-31 10:05:55 -0700

    added dictionary

--- solution.py
+++ solution.py
@@ -1,17 +1,30 @@
+
+language = input("What language does the person speak?").lower()
+# using a dictionary
+dictionary = {
+  "english": "Hello!",
+  "spanish": "Hola!",
+  "french": "Bonjour!",
+  "german": "Hallo!"
+}
+
+print(dictionary[language])
+
 operation = input("Say hello in English, French, German or Spanish:")
+# you can add .low to line above to deal with upper and lower case inputs
 
 # English
 if operation == "hello" or operation == "Hello":
-    print(f"Good morning!")
+    print(f"Good morning! 🇺🇸 🇬🇧 🏴󠁧󠁢󠁳󠁣󠁴󠁿")
 #else if French
 elif operation == "bonjour" or operation == "Bonjour":
-    print(f"Bonne matinée!")
+    print(f"Bonne matinée! 🇫🇷 🇨🇩 🇨🇦")
 #else if German
 elif operation == "hallo" or operation == "Hallo":
-    print(f"Guten Morgen!")
+    print(f"Guten Morgen! 🇩🇪 🇦🇹 🇨🇭")
 #else if Spanish
 elif operation == "hola" or operation == "Hola":
-    print(f"Buenos días!")
+    print(f"Buenos días! 🇪🇸 🇲🇽 🇦🇷")
 #catch
 else:
     print("Incorrect input 🤔 ")
