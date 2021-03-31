c3ffc7ebd67b25fc6dad583c4c95f9468fd7345c
NBryant83 <nakisha.bryant@gmail.com>
2021-03-31 11:57:15 -0400

    hw commit

---
+++ solution.py
@@ -0,0 +1,15 @@
+# write a function
+# parameter for language
+
+language = input("What language would you like to use? ")
+      
+if language == "english":
+  print("Hello!")
+elif language == "spanish":
+  print("Hola!")
+elif language == "french":
+  print("Bonjour!")
+
+
+
+
